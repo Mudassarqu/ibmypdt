a2864c4a13be50c7802de9ca680c84b4a804ef8f
Mudassar Qureshi <mudassarqu@gmail.com>
2021-12-25 23:51:53 +0530

    init

--- src/About.js
+++ src/About.js
@@ -1,6 +1,5 @@
 
 import React from "react";
-import {Route,Switch} from "react-router-dom";
 function About() {
 
   return <h1>ABOUT PAGE</h1>
--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import {Route,Switch} from "react-router-dom";
+
 function Contact() {
 
   return <h1>CONTACT PAGE</h1>
