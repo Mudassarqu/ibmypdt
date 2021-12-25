0c5034db6ad052a6213368674ef61cdd1c7c8179
Mudassar Qureshi <mudassarqu@gmail.com>
2021-12-25 23:33:38 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 
 import React from "react";
+import {Route,Switch} from "react-router-dom";
+import About from './About';
+import Contact from './Contact';
+import Error from './Error';
 
 function App() {
 
-  return <h1>My name is Mudassar</h1>
+  return(
+    <>
+<Switch>
+<Route exact path="/" component={About} />
+<Route path="/contact" component={Contact} />
+<Route component={Error}/>
+</Switch>
+  
+  
+  </>
+
 };
 export default App;
-
-/*import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-     <h1>Mudassar Qureshi</h1>
-    <h2>Taif, ATIF & MUSAID Testing</h2>
-    </div>
-  );
-}
-
-export default App;
-*/
