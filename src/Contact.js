63ca63ebc04d6ce248ea141ae8ba6c5e7f104e9b
Mudassar Qureshi <mudassarqu@gmail.com>
2021-12-25 23:33:47 +0530

    init

---
+++ src/Contact.js
@@ -0,0 +1,8 @@
+
+import React from "react";
+import {Route,Switch} from "react-router-dom";
+function Contact() {
+
+  return <h1>CONTACT PAGE</h1>
+};
+export default Contact;
---
+++ src/Error.js
@@ -0,0 +1,8 @@
+
+import React from "react";
+import {Route,Switch} from "react-router-dom";
+function About() {
+
+  return <h1>OOPS ! Page not found</h1>
+};
+export default About;
