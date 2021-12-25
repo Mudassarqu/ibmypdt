97c8b96702c42f76836259ce07b103dd0c1fbbc4
Mudassar Qureshi <mudassarqu@gmail.com>
2021-12-25 23:39:09 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import {Route,Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import About from './About';
 import Contact from './Contact';
 import Error from './Error';
